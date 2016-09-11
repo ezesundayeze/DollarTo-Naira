371a4e285d5783cddf8753d1f1a93d16a48b6b8c
Eze Sunday <rexeze@gmail.com>
2016-09-10 17:13:39 -1200

    initial commit

---
+++ Currency Exchanger app/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Currency_Exchanger_app
+{
+    class Program
+    {
+        public class CurrencyExchange
+        {
+           
+            public double timestamp{ get; set; }
+            public string Base { get; set; }
+            public Dictionary<object, object> rates { get; set; }  
+        }
+
+       
+
+        static void Main(string[] args)
+        {
+            using(var webs =new  WebClient()){
+                try
+                {
+                    var OpenExchange = webs.DownloadString("https://openexchangerates.org/api/latest.json?app_id=c6e479aeb7b34e3bab041d8c0772b6ba");
+                    var Deserializenw = JsonConvert.DeserializeObject<CurrencyExchange>(OpenExchange);
+                    Console.WriteLine("The Dollar exchange rate to Naira is {0}/$  as of {1}",Deserializenw.rates["NGN"],Deserializenw.timestamp);
+                }catch(Exception ex){
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
